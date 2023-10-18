ab84ff8155367a60ef61948a091d85bf74e8f020
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2023-10-18 09:57:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import React from "react";
-
-for (var i = 3; i < document.querySelectorAll('div').length - 1; ++i) {
-  document.querySelectorAll('div')[i].addEventListener("click", function () {
-    var newClass = this.id;
-    document.querySelector('button').id=newClass;
-    document.querySelector('button').innerHTML=newClass;
-  });
+import React, { useState } from "react";
+function displayColor() {
+  for (var i = 3; i < document.querySelectorAll('div').length - 1; ++i) {
+    document.querySelectorAll('div')[i].addEventListener("click", function () {
+      var newId = this.id;
+      document.querySelector("button").style.background = newId;
+      document.querySelector('button').innerHTML = newId;
+    });
+  }
+}
+function showOption(color){
+  displayColor();
+  return (
+    <div id={color} ></div>
+  );
 }
 
-function App() {
-
+function App({props}) {
+  
+  const [isVisible,isvalid]=useState(false);
   return (
 
     <center>
       <h1>Colour Picker</h1>
       <div className="box">
-        <div className="box1">
-          <div id="indigo"></div>
-          <div id="orange"></div>
-          <div id="maroon"></div>
-          <div id="olive"></div>
-          <div id="voilet"></div>
-          <div id="wheat"></div>
-          <div id="pink"></div>
-          <div id="yellow"></div>
-          <div id="red"></div>
-          <div id="aqua"></div>
-          <div id="black"></div>
-          <div id="blue"></div>
-          <div id="chocolate"></div>
-          <div id="darkgreen"></div>
-          <div id="darkblue"></div>
-          <div id="darkvoilet"></div>
-          <div id="brown"></div>
-          <div id="cblue"></div>
-          <div id="corol"></div>
+        <div className="box1" style={{visibility:isVisible? "visible":"hidden"}}>
+          {props.map(showOption)}
         </div>
-        <div className="button" ><button id='btn'>Pick A Colour</button></div>
+        <div className="button" ><button id='btn' onClick={()=>isvalid(true)}>Pick A Colour</button></div>
 
       </div>
     </center>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-
+var Colour=["indigo","orange","maroon","olive","violet","wheat","pink","yellow","red","aqua","black","blue","chocolate","darkgreen","darkblue","darkviolet","brown","cadetblue","coral"]
 
 ReactDOM.render(
- <App />,document.getElementById('root')
+ <App props={Colour}/>,document.getElementById('root')
 );
 
 
